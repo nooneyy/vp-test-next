dea572249eadd82140c7de73bf2d1fab74568f77
Tomas Kolarik <tomaskolarik1@seznam.cz>
2024-07-08 15:22:05 +0200

    feat: add icons to product sections

--- components/store/products/product-section.tsx
+++ components/store/products/product-section.tsx
@@ -3,6 +3,7 @@ import {
   ProductSkeleton,
   type SmallProduct,
 } from "@/components/store/products/product";
+import { SquarePlus, TrendingUp } from "lucide-react";
 
 export const ProductSection = ({
   name,
@@ -42,8 +43,8 @@ const ProductSectionWrapper = ({
 }) => {
   return (
     <section className="flex flex-col items-center justify-center pt-10 space-y-10 md:px-[4%] lg:px-[7%] xl:px-[8%] 2xl:px-[10%]">
-      <p className="text-3xl lg:text-4xl font-semibold tracking-tight lg:self-start">
-        {name}
+      <p className="text-3xl lg:text-4xl font-semibold tracking-tight lg:self-start flex items-center gap-2 lg:ml-4">
+        {productSectionBadge(name)}{name}
       </p>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 md:gap-16 lg:gap-12 xl:gap-16">
         {children}
@@ -51,3 +52,15 @@ const ProductSectionWrapper = ({
     </section>
   );
 };
+
+const productSectionBadge = (name: string) => {
+  switch (name) {
+    case "Novinky": {
+      return <SquarePlus className="size-7" />;
+    }
+    case "Nejprodávanější": {
+      return <TrendingUp className="size-7" />;
+    }
+    default: return null;
+  }
+}
